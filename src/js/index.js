5e6baf7c4796ee1381427a90dcb315a5a203e2bc
ilmirazaynetdinova <ilmirazainetdinova686@gmail.com>
2020-09-28 12:08:53 +0300

    o

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,5 @@
 import '../scss/main.scss'
 
-    let element = document.querySelector('.btn');
-    let hiddenContent = document.querySelectorAll('.brands-one-hidden')
-    let btn = document.querySelector('.btn-brands');
-    element.addEventListener('click', () => {
-        btn.textContent === 'Показать все'
-            ? btn.innerHTML = 'Скрыть'
-            : btn.innerHTML = 'Показать все'
-
-        hiddenContent.forEach((element) => {
-            element.classList.toggle('hidden')
-        })
-    })
-
-    let burgerMenu = document.querySelector('.burger-menu');
-    let button = document.querySelector('.burger-menu__button');
-    button.addEventListener('click',() => {
-        burgerMenu.classList.toggle('burger-menu_active')
-    })
 
 
 
